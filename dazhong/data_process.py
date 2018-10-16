1b93199f86d1627b15f8709fa2b490b5e44caf1d
jingshuiliushen <2550561233@qq.com>
2018-10-16 18:29:23 +0800

    标签值处理
    根据stars列生成target列，并去掉中评的数据

--- dazhong/data_process.py
+++ dazhong/data_process.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python  
 # encoding: utf-8 
 import pandas as pd
+def getLabel(score): 
+    if score > 3: 
+        return 1 
+    elif score < 3:
+        return 0 
+    else: 
+        return None 
+    
 def loaddata():
     #显示所有列
     pd.set_option('display.max_columns', None)
     pd.set_option('display.width',200)
     data=pd.read_csv('data/data.csv')
-    print(data.describe(percentiles=None, include=None, exclude=None))
-
-
+    return data
+def createLabel(data):
+    data['target'] = data['stars'].map(lambda x:getLabel(x))
+    return data.dropna()
+def preprocess(data):
+    data=createLabel(data)
+    return data
 if __name__ == '__main__':
-    loaddata()
+    data=loaddata()
+    data=preprocess(data)
+    print(data.info())
